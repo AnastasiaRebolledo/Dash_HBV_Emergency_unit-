89b6c91c01b5245abe546498e0a03b73ff4008c7
AnastasiaRebolledo <anastasia.rebolledo.r@gmail.com>
2023-01-27 17:44:54 -0300

    machine learning

--- Prueba/machine_learning.R
+++ Prueba/machine_learning.R
@@ -1,9 +1,5 @@
-lunes_inicio_sem1 <- as.POSIXct("2022-08-01 08:00:00")
-lunes_termino_sem1 <- as.POSIXct("2022-08-01 20:00:00")
 
-lunes_sem1<-data.frame("dias"=seq(from=lunes_inicio_sem1, to=lunes_termino_sem1, by="360 min"))
-
-semana1<-rbind(lunes_sem1)
+## fila 1 a 1169 corresponde a entrenamiento, 1170 hasta 1461 validación
 
 #install.packages("h2o")
 #install.packages("timetk")
@@ -14,29 +10,34 @@ library(timetk)
 library(dplyr)
 
 #transformacion del mes segun el timetk
-mes<-semana1%>%tk_augment_timeseries_signature()
+demanda.diaria<-data%>%tk_augment_timeseries_signature()
 
 #limpiar los NA
-mes<-mes%>%
+demanda.diaria<-demanda.diaria%>%
   select_if(~!any(is.na(.)))%>%
   mutate_if(is.ordered,~as.character(.)%>%as.factor)
 
-#generacion del set de datos train y valid
-train<-mes[1:45,]
-valid<-mes[46:60,]
+demanda.diaria<-cbind(data$demanda,demanda.diaria)
+#colnames(demanda.diaria$`data$demanda`)<-c("demanda")
 
-h2o.init(max_mem_size =  "10g",min_mem_size = "1g")
+#generacion del set de datos train y valid
+train<-demanda.diaria[1:1169,]
+valid<-demanda.diaria[1170:1461,]
+
+h2o.init(max_mem_size =  "1g",min_mem_size = "1g")
 
 #transformar el data.frame en un formato h2o
 train_h2o<-as.h2o(train)
 valid_h2o<-as.h2o(valid)
 
-dl <- h2o.deeplearning(x = "variable x",
-                       y = "variable y",
-                       hidden = c(5,5),
+dl <- h2o.deeplearning(x = 3:29,
+                       y = "data$demanda",
+                       training_frame = train_h2o,
+                       hidden = c(200,200),
                        epochs = 1000,
                        activation = "Tanh",
                        seed = 23123)
 
-predic_train_bike_rn_est3<-h2o.predict(train_rn_model_bike_est3,valid_h2o)
-predic_train_bike_rn_est3<-as.data.frame(predic_train_bike_rn_est3)
+predicción<-h2o.predict(dl,valid_h2o)
+predicción<-as.data.frame(predicción)
+compara<-data.frame(predicción,valid$`data$demanda`)
